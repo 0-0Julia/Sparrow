56039df2dcefb64de45fac223aa68a30916b0c02
Julia Peisahovich <juliapeisahovich@gmail.com>
2023-02-25 09:46:51 +0200

    Add a post

---
+++ AddPost.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AddPost.aspx.cs" Inherits="Birds_Guide.AddPost" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form  runat="server" style="width:70%; align-content:center" class="form">
+        <fieldset class="">
+
+        <legend>Title:</legend><input required class="input" Id="title" name="title" maxlength="50"/><p id="messege" style="color:red" ></p>
+
+            <label>Base Image:<asp:FileUpload Id="baseimg" runat="server"/></label>
+          
+            <label>category </label><select id="category" name="category">
+                <option id="Home" value="Home">Home</option>
+                <option id="Farm" value="Farm">Farm</option>
+                <option id="Exotics" value="Exotics">Exotics</option>
+                                    </select><br />
+         <label>Description:</label>   
+        <textarea Id="desc" name="desc" maxlength="4000" style="width: 406px; height: 142px;"></textarea><p style="color:red"     ></p><br />
+                                                     
+            
+             <label>Care:<asp:FileUpload Id="first" runat="server"/></label>   
+        <textarea required Id="care" name="care" maxlength="8000" style="width: 406px; height: 142px;"></textarea><p style="color:red"     ></p><br />
+         
+
+             <label>secon image:<asp:FileUpload Id="second" runat="server"/></label> <label>Illnesses:</label>   
+        <textarea required Id="ill" name="ill" maxlength="4000" style="width: 406px; height: 142px;"></textarea><p style="color:red" ></p><br />
+                                                    
+                   
+            <label>How to choose?<asp:FileUpload Id="third" runat="server"/></label>   
+        <textarea required Id="choo" name="choo" maxlength="4000" style="width: 406px; height: 142px;"></textarea><p style="color:red" > </p><br />
+                                                                                           
+            <label>Fun Facts:<asp:FileUpload Id="forth" runat="server"/></label>   
+        <textarea required Id="fact" name="fact" maxlength="4000" style="width: 406px; height: 142px;"></textarea><p style="color:red"></p><br />
+        
+        <asp:Button Id="post" runat="server" onclick="post_Click" Text="Upload the Post" />
+   </fieldset> </form>
+</asp:Content>
+
---
+++ AddPost.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Birds_Guide
+{
+
+    public partial class AddPost : System.Web.UI.Page
+    {
+        public int author_id;
+        public string des;
+        public string care;
+        public string ill;
+        public string cho;
+        public string fact;
+        public string baseimgPath;
+        public string firstimgPath;
+        public string secondimgPath;
+        public string thirdimgPath;
+        public string forthimgPath;
+        public string category;
+        public string title;
+        static DataTable userD;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["admin"] == null && Session["writer"] == null)
+                Response.Redirect("Homepage.aspx");
+        }
+
+        protected void post_Click(object sender, EventArgs e)
+        {
+            userD = MyAdoHelper.ExecuteUser((string)Session["username"]);
+            author_id = (int)MyAdoHelper.ExecuteUser((string)Session["username"]).Rows[0]["Id"];
+            des = Request.Form["desc"];
+            ill = Request.Form["ill"];
+            fact = Request.Form["fact"];
+            cho = Request.Form["choo"];
+            care = Request.Form["care"];
+            category= Request.Form["category"];
+            title= Request.Form["title"];
+            if (baseimg.HasFile)
+            {
+                string basefilePath = Server.MapPath("pictures/postPic/" + System.Guid.NewGuid() + Path.GetExtension(baseimg.FileName));
+                baseimg.SaveAs(basefilePath);
+                baseimgPath = basefilePath.Substring(basefilePath.LastIndexOf("\\"), basefilePath.Length - basefilePath.LastIndexOf("\\")).Remove(0, 1);
+            }
+                
+            if (first.HasFile)
+            {
+                string filePath = Server.MapPath("pictures/postPic/" + System.Guid.NewGuid() + Path.GetExtension(first.FileName));
+                first.SaveAs(filePath);
+                int getPos = filePath.LastIndexOf("\\");
+                int len = filePath.Length;
+                firstimgPath = filePath.Substring(getPos, len - getPos).Remove(0, 1);
+            }
+            if (second.HasFile)
+            {
+                string filePath = Server.MapPath("pictures/postPic/" + System.Guid.NewGuid() + Path.GetExtension(second.FileName));
+                first.SaveAs(filePath);
+                int getPos = filePath.LastIndexOf("\\");
+                int len = filePath.Length;
+                secondimgPath = filePath.Substring(getPos, len - getPos).Remove(0, 1);
+            }
+            if (third.HasFile)
+            {
+                string filePath = Server.MapPath("pictures/postPic/" + System.Guid.NewGuid() + Path.GetExtension(third.FileName));
+                third.SaveAs(filePath);
+                int getPos = filePath.LastIndexOf("\\");
+                int len = filePath.Length;
+                thirdimgPath = filePath.Substring(getPos, len - getPos).Remove(0, 1);
+            }
+            if (forth.HasFile)
+            {
+                string filePath = Server.MapPath("pictures/postPic/" + System.Guid.NewGuid() + Path.GetExtension(forth.FileName));
+                forth.SaveAs(filePath);
+                int getPos = filePath.LastIndexOf("\\");
+                int len = filePath.Length;
+                forthimgPath = filePath.Substring(getPos, len - getPos).Remove(0, 1);
+            }
+            MyAdoHelper.AddPost(title, des, care, ill, cho, fact, baseimgPath, firstimgPath, secondimgPath, thirdimgPath, forthimgPath, author_id, category);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Post was added successfuly');", true);
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
