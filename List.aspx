1a20610816de41a5bab6222b6f13fbf72e0d132d
Julia Peisahovich <juliapeisahovich@gmail.com>
2023-02-25 09:48:01 +0200

    List of Posts

---
+++ List.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Our List Of Birds" Language="C#" MasterPageFile="Site1.Master" AutoEventWireup="true" CodeBehind="List.aspx.cs" Inherits="Birds_Guide.List" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="containerL">
+			<h2 class="collection-title">Our List Of Birds <small>Only Quality Content</small></h2>
+           <%=st %>
+    </div>
+</asp:Content>
---
+++ List.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Birds_Guide
+{
+    public partial class List : System.Web.UI.Page
+    {
+        public string st;
+        static DataTable img;
+        static DataTable content;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            img = MyAdoHelper.ExecuteAllBirdsImg();
+            ListLoad();
+        }
+        protected void ListLoad()
+        {
+            for (int i = 0; i < img.Rows.Count; i++)
+            {
+                st += "<div class='collections'>";
+                st += "<div class='collection-item-outer'>";
+				st += "<div class='collection-item'>";
+                st += "<img src = 'pictures/postPic"+img.Rows[i]["BaseImg"]+"' alt='an image of "+img.Rows[i]["BirdName"]+"' />";
+                st += "<div class='collection-text'>";
+				st += "<h3>"+img.Rows[i]["BirdName"]+"</h3>";
+                content = MyAdoHelper.ExecuteBirdsContent((string)img.Rows[0]["BirdName"]);
+                st += "<p class='List_p'>"+content.Rows[0]["DescriptionT"]+"</p>";
+                st += "<button> Read More...</button></div></div></div></div>";
+            }
+        }
+    }
+}
