0995379de04d0438e7eada80ef9b2c3d674feed7
Julia Peisahovich <juliapeisahovich@gmail.com>
2023-02-25 09:45:14 +0200

    Log Out

---
+++ Log_out.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Birds_Guide
+{
+    public partial class Log_out : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+                Response.Redirect("HomePage.aspx");
+            Session.Abandon();
+
+            Application.Lock();
+            Application["userCount"] = (int)Application["userCount"] - 1;
+            Application.UnLock();
+            HttpCookie cookies = Request.Cookies["cookies"];
+            cookies.Expires = DateTime.Now.AddMinutes(-10);
+            Response.Redirect("HomePage.aspx");            
+        }
+        
+    }
+}
