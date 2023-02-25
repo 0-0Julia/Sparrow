7dbf7553622f7f4b1ef289d47e5363d53088a6fe
Julia Peisahovich <juliapeisahovich@gmail.com>
2023-02-25 09:42:20 +0200

    Sign in

---
+++ SignIn.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page  Title="Pets guide | Sign in" Language="C#" MasterPageFile="Site1.Master"  AutoEventWireup="true" CodeBehind="SignIn.aspx.cs" Inherits="Birds_Guide.SignIn" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server"> 
+    <script src="JavaScript.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <header>
+        <h1>SIGN IN</h1>
+    </header>
+
+    <div class="containerS">
+     <fieldset>
+            <legend>SIGN IN:</legend>
+           <form class="form" runat="server">
+             <%=message %>
+                <label>Username:</label>
+                    <input class="input" type="text" id="username" name="username" onchange="inputusername()"/>
+                            <p id="UsernameP" class="pStyle"></p> 
+               <label>Password:</label>
+                        <input class="input" type="password" id="password" name="password" onchange="inputpassword()"/>
+                               <p  id="PassP" class="pStyle""></p> 
+              
+              <br />  <label>
+               show password<input type="checkbox" onclick="passwordTransform()"> 
+             </label>
+                <label style="color:snow">
+                    <input class="btn add" type="submit" id="submit" name="submit" value="Sign In" onclick="return click()"/>
+                </label>
+                               <a href="SignUp.aspx" class="underline-animation" style="text-align:center;">doesn't have an account yet?</a><br />
+               </form>
+        </fieldset>  
+     </div> 
+</asp:Content>
---
+++ SignIn.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Birds_Guide
+{
+    
+    public partial class SignIn : System.Web.UI.Page
+    {
+        DataTable dt;
+        private string username, password;
+        public string message = "";
+        public string usernamefield;
+        public string passwordfield;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] != null)
+                Response.Redirect("HomePage.aspx");
+            else 
+            {
+                HttpCookie cookies = Request.Cookies["cookies"];
+                if (cookies != null)
+                {
+                    passwordfield = cookies["passwordC"];
+                    usernamefield = cookies["usernameC"];
+                }
+            }          
+
+            if (Request.Form["submit"] != null)
+            {
+                GetDataFromUser();
+                GetDataTableFromDataBase();
+
+                if (dt.Rows.Count ==0)
+                    message = "wrong username or password, please try again";
+                else
+                {
+                    Session["username"] = username;
+                    
+                    if(MyAdoHelper.IsAdmin((string)Session["username"]))
+                         Session["admin"] = 1;
+                    if (MyAdoHelper.IsWriter((string)Session["username"]))
+                        Session["writer"] = 1;
+                    Application.Lock();
+                    if (Application["userCount"] == null)
+                        Application["userCount"] = 0;
+                    Application["userCount"] = (int)Application["userCount"] + 1;
+                    Application.UnLock();
+                    
+                    HttpCookie cookies = new HttpCookie("cookies");
+                    cookies["usernameC"] = username;
+                    cookies["passwordC"] = password;
+                    cookies.HttpOnly = true;
+                    cookies.Secure = true;
+                    Response.Cookies.Add(cookies);
+                    Response.Redirect("HomePage.aspx");
+                }
+            }
+        }
+        private void GetDataFromUser()
+        {
+            username = Request.Form["Username"];
+            password = Request.Form["Password"];
+        }
+        private void GetDataTableFromDataBase()
+        {
+            dt = MyAdoHelper.MatchUser(username, password);
+            
+        }
+    }
+}
